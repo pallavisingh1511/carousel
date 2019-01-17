54696b793a48d5fb76a35fbcb08e015c86b04d8c
Pallavi Singh <pallavi.singh@outlook.in>
2019-01-18 04:49:42 +0530

    Carousel feature added

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,13 +1,75 @@
 import React, { Component, Fragment } from 'react'
+import Slider from "react-slick";
+import { NUMBERS } from './../../utils/constants'
+import style from './style.less'
 
 class Profile extends Component {
+    constructor() {
+        super()
+        this.state = {
+            selectedValue: 1
+        }
+    }
     
+    _renderCarousel() {      
+        let slides = []
+        for (let i = 1; i <= this.state.selectedValue; i++) {
+            let color = "#" + ((1<<24) * Math.random()|0).toString(16)
+            slides.push(
+                <div key={ i }>
+                    <div className={ style.slide } style={{background: color,}}>
+                        <span>{ NUMBERS[i] }</span>
+                    </div>
+                </div>
+            )
+        }
+        return slides
+    }
+
+    _renderOptions() {
+        let options = []
+        for (let i = 1; i <= 20; i++) {
+            options.push(<option value={ i } key={ i }>{ i }</option>)
+        }
+        return options
+    }
+
+    _setSelected(e) {
+        this.setState({
+            selectedValue: e.target.value
+        })
+    }
+
     render() {
         const { user } = this.props
+        
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+        }
         return(
-            <div >
-                Welcome { user }!
-            </div>
+            <Fragment>
+                <div className={ style.header }>
+                    Welcome { user }!
+                </div>
+
+                <select 
+                    value={ this.state.selectedValue } 
+                    onChange={ (e) => this._setSelected(e) } 
+                >
+                    { this._renderOptions() }
+                </select>
+                <div className={ style.slickContainer }>
+                    <Slider {...settings}>
+                        {
+                            this._renderCarousel()
+                        }
+                    </Slider>
+                </div>
+            </Fragment>
         )
     }
 }
